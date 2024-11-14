1df72e4977f2b380e6961549377aad8d40761de6
Amanda Meneghini <admcmarshmallow@gmail.com>
2024-11-14 12:52:56 -0300

    deploy with react test

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -10,8 +10,12 @@ export const ButtonContainer = styled.button`
     font-weight: 700;
     flex: 1;
     border-radius: 10px;
+    scale: 1;
+    transition: all 0.5s ease-out;
 
     &: hover{
         opacity: 0.8;
+        scale: 1.1;
+        transition: easy in out;
     },
 `
